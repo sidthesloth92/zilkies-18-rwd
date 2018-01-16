66e7ffef348fe6591e922be1fba0c18b66d44687
Raxit Jain <rjain@ztech.io>
2018-01-16 12:58:55 +0530

    "Added complete to-do list"

--- Raxit/JS/dynamic-list.js
+++ Raxit/JS/dynamic-list.js
@@ -1,8 +1,7 @@
-var taskList = document.getElementsByClassName("list");
-var taskListObject = {};
-var id;
+var jsonDataList = document.getElementsByClassName("jsonDataList");
+var fragments = document.createDocumentFragment();
+var listObject = {};
 
-//For getting uniqueID for each task.
 var uniqueID = (function() {
     var id = 0;
     return function() {
@@ -10,74 +9,89 @@ var uniqueID = (function() {
     };
 })();
 
-//Constructor for initializing the object.
-function TaskManager(taskName, taskID, taskDescription) {
-    this.taskName = taskName;
-    this.taskID = taskID;
-    this.taskDescription = taskDescription;
+function Manager(name, id) {
+    this.name = name;
+    this.id = id;
+}
+
+window.onload = function() {
+    if(confirm("Do you want to fetch the JSON data from external URL?") == true) {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                var json = JSON.parse(xhttp.responseText);
+                insertReply(json);
+            }
+        };
+        xhttp.open("GET", "https://jsonplaceholder.typicode.com/users", true);
+        xhttp.send();
+    }
+}
+
+function insertReply(content) {
+    for(var i = 0;i < content.length; i++) {
+        var taskObject = new Manager(content[i].name, uniqueID());
+        insertFragment(taskObject);
+    }
+    jsonDataList[0].appendChild(fragments);
+}
+
+function insertFragment(taskObject) {
+    listObject[taskObject.id] = taskObject;
+    var nameText = document.createElement("p");
+    var userListItem = document.createElement("li");
+    var removeButton = document.createElement("button");
+    var uncheckButton = document.createElement("button");
+    nameText.appendChild(document.createTextNode("Name : " + taskObject.name));
+    removeButton.classList.add('remove-buttons-style');
+    uncheckButton.classList.add('uncheck-buttons-style');
+    removeButton.appendChild(document.createTextNode("X"));
+    removeButton.setAttribute('id',"remove-" + taskObject.id);
+    uncheckButton.appendChild(document.createTextNode("C"));
+    uncheckButton.setAttribute('id', 'uncheck-' + taskObject.id);
+    userListItem.appendChild(removeButton);
+    userListItem.appendChild(uncheckButton);
+    userListItem.appendChild(nameText);
+    userListItem.classList.add('div-styling');
+    fragments.appendChild(userListItem);
 }
 
 //onClick() listener for adding elements in the unordered list.
 function myFunction() {
     var taskName = document.getElementById("task");
-    var description = document.getElementById("description");
     var errorMessage = document.getElementsByClassName("error-message");
-    //Validation for the input tesxt boxes.
+    //Validation for the input text boxes.
     var taskNameValue = taskName.value.trim();
-    var descriptionValue = description.value.trim();
-    if(taskNameValue.length == 0 || descriptionValue.length == 0) {
+    if(taskNameValue.length == 0) {
         errorMessage[0].innerHTML = "Please enter all the fields.";
         taskName.value = "";
-        description.value = "";
     }
     else {
         errorMessage[0].innerHTML = "";
-        //Adding object to the global object.
-        id = uniqueID();
-        var taskObject = new TaskManager(taskName.value, id, description.value);
-        taskListObject["Task-" + id] = taskObject;
-        //Making the unordered flex list
-        var division = document.createElement("div");
-        var listItem = document.createElement("li");
-        var removeButton = document.createElement("button");
-        var uncheckButton = document.createElement("button");
-        var fragments = document.createDocumentFragment();
-        listItem.appendChild(document.createTextNode(taskObject.taskName));
-        removeButton.appendChild(document.createTextNode("X"));
-        removeButton.classList.add('remove-buttons-style');
-        removeButton.setAttribute('id',"remove-" + id);
-        uncheckButton.appendChild(document.createTextNode("C"));
-        uncheckButton.classList.add('uncheck-buttons-style');
-        uncheckButton.setAttribute('id', 'uncheck-' + id);
-        division.appendChild(removeButton);
-        division.appendChild(uncheckButton);
-        division.appendChild(listItem);
-        division.classList.add('div-styling');
-        fragments.appendChild(division);
-        taskList[0].appendChild(fragments);
-        //Emptying the input text boxes after the completion of operation.
+        var taskObject = new Manager(taskName.value, uniqueID());
+        insertFragment(taskObject);
         taskName.value = "";
-        description.value = "";
     }
+    jsonDataList[0].appendChild(fragments);
 }
 
-//onClick() listener for deleting and striking list items
-//This function also deletes the object in the object array.
+//onClick() listener for deleting and striking user list items
+//This function also deletes the object in the User List object array.
 function list(event) {
     var getId = (event.target.id.split("-"));
     var listItem = document.getElementById(event.target.id);
-    var taskObject = {};
-    taskObject = taskListObject["Task-" + getId[1]];
+    var object = {};
+    object = listObject[getId[1]];
     if(getId[0] == "uncheck") {
-        if(listItem.parentNode.lastChild.classList.contains('uncheck')){
-            listItem.parentNode.lastChild.classList.remove('uncheck');
+        if(listItem.parentNode.classList.contains('uncheck')){
+            listItem.parentNode.classList.remove('uncheck');
         } else {
-            listItem.parentNode.lastChild.classList.add('uncheck');
+            listItem.parentNode.classList.add('uncheck');
         }
     } else if(getId[0] == "remove") {
-        if(confirm("Do you want to delete " + taskObject.taskName + "?") == true) {
+        if(confirm("Do you want to delete " + object.name + "?") == true) {
             listItem.parentNode.parentNode.removeChild(listItem.parentNode);
-            delete taskListObject["Task-" + getId[1]];
+            delete listObject[getId[1]];
         }
     }
 }
